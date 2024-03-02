2c8768196490ad5d2ae35d0c0b0a771053534dd4
blackerby <wmblackerby@gmail.com>
2024-03-01 21:42:01 -0500

    TabWriter and comfy_table are working; next stop: feature flagging

--- sitemaps-cli/src/main.rs
+++ sitemaps-cli/src/main.rs
@@ -1,10 +1,13 @@
 pub mod cli;
 
+use std::io::Write;
+
 use crate::cli::Cli;
 use clap::Parser;
 use comfy_table::Table;
 use sitemaps::error::SitemapError;
 use sitemaps::reader::SitemapReader;
+use tabwriter::TabWriter;
 
 fn main() -> Result<(), SitemapError> {
     let cli = Cli::parse();
@@ -13,20 +16,23 @@ fn main() -> Result<(), SitemapError> {
         let sitemap = SitemapReader::read(&path)?;
         let mut table = Table::new();
         table.set_header(vec!["loc", "lastmod", "changefreq", "priority"]);
+        let mut lines = String::new();
+
+        let mut tw = TabWriter::new(vec![]);
 
         for url in sitemap.urlset.0 {
-            let mut line = String::from(&url.loc);
+            lines.push_str(&url.loc.to_string());
 
             if cli.lastmod {
-                line.push_str(&format!("\t{}\t", url.last_mod.unwrap()));
+                lines.push_str(&format!("\t{}\t", url.last_mod.unwrap()));
             }
 
             if cli.changefreq {
-                line.push_str(&format!("{}\t", url.change_freq.unwrap()));
+                lines.push_str(&format!("{}\t", url.change_freq.unwrap()));
             }
 
             if cli.priority {
-                line.push_str(&format!("{}", url.priority.unwrap()));
+                lines.push_str(&format!("{}\t", url.priority.unwrap()));
             }
 
             table.add_row(vec![
@@ -35,11 +41,15 @@ fn main() -> Result<(), SitemapError> {
                 url.change_freq.unwrap().to_string(),
                 url.priority.unwrap().to_string(),
             ]);
-
-            println!("{line}");
+            lines.push('\n');
         }
 
-        // println!("{table}");
+        tw.write_all(lines.as_bytes()).unwrap();
+        tw.flush().unwrap();
+        let output = String::from_utf8(tw.into_inner().unwrap()).unwrap();
+        print!("{output}");
+
+        println!("{table}");
     }
 
     Ok(())
