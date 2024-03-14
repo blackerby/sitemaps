c100e36be0e142ff78f94976ede008b8636c1f4e
blackerby <wmblackerby@gmail.com>
2024-03-13 20:39:41 -0400

    Command name configuration and args documentation

--- sitemaps-cli/src/cli.rs
+++ sitemaps-cli/src/cli.rs
@@ -1,26 +1,35 @@
 use clap::Parser;
 
 #[derive(Parser)]
-#[command(name = "Simple Sitemap")]
+#[command(name = "Sitemaps")]
 #[command(version = "0.1.0")]
 #[command(about = "Read data from sitemap.xml files", long_about = None)]
 pub(crate) struct Cli {
+    /// Path to sitemap.xml file
     #[arg(default_value = "-")]
     pub path: Option<String>,
+    /// Include value of <loc> in output
     #[arg(short, long, default_value_t = true)]
     pub loc: bool,
-    #[arg(short = 'L', long)]
+    /// Include value of <lastmod> in output
+    #[arg(short = 'L', long, default_value_t = true)]
     pub lastmod: bool,
+    /// Include value of <changefreq> in output. Sitemaps only.
     #[arg(short, long)]
     pub changefreq: bool,
+    /// Include value of <priority> in output. Sitemaps only.
     #[arg(short, long)]
     pub priority: bool,
+    /// Print output table with cell borders.
     #[arg(short = 'P', long)]
     pub pretty: bool,
+    /// Print output table with column headers.
     #[arg(short = 'H', long, default_value_t = true)]
     pub header: bool,
+    /// Print output as JSON.
     #[arg(short, long)]
     pub json: bool,
+    /// Print output as CSV.
     #[arg(short = 'C', long)]
     pub csv: bool,
 }
